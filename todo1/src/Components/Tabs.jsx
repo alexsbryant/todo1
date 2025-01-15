1688f51bed9fea90efd351c0bb4d6290d4ba964c
Alexander Bryant <asbryant@me.com>
2025-01-15 16:53:02 -0700

    Update Tabs.jsx
    Tabs jsx updated

--- todo1/src/Components/Tabs.jsx
+++ todo1/src/Components/Tabs.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
 function Tabs() {
+  const tabs = ['All', 'Open', 'Completed'];
   return (
-    <nav></nav>
+    <nav className="tab-container">
+      {tabs.map((tab, tabIndex) => {
+        return (
+          <button 
+          key={tabIndex}
+          className="tab-button">
+            <h4>{tab} <span>(0)</span></h4>
+          </button>
+        )
+      })}
+    </nav>
   )
 }
 
