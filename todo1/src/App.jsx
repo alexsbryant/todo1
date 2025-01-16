722d6f04d61ea944df57126bbc7e64a05403656a
Alexander Bryant <asbryant@me.com>
2025-01-16 09:53:10 -0700

    Update App.jsx
    Added handler functions and imported use state

--- todo1/src/App.jsx
+++ todo1/src/App.jsx
@@ -1,15 +1,33 @@
+import React, { useState } from "react";
 import Header from "./Components/Header";
 import Tabs from "./Components/Tabs";
 import TodoInput from "./Components/TodoInput";
 import ToDoList from "./Components/ToDoList";
 
 function App() {
-  const todos = [
+/*   const todos = [
     { input: 'Hello! Add your first todo!', complete: true },
     { input: 'Get the groceries', complete: false },
     { input: 'Take the dog out', complete: false },
     { input: 'Wash the car', complete: true }
-  ]
+  ] */
+
+  const [todos, setTodos] = useState([
+    { input: 'Hello! Add your first todo!', complete: true }
+  ]);
+
+  function handleAddTodo() {
+    
+  };
+
+  function handleEditTodo() {
+
+  };
+
+  function handleDeleteTodo() {
+
+  };
+
   return (
     <>
       <Header todos={todos} />
