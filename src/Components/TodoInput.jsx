8632f51eac5c8a82e43e9c38d9b637acbca7a534
Alexander Bryant <asbryant@me.com>
2025-01-16 12:30:13 -0700

    Update TodoInput.jsx
    Added 'return / enter' press functionality

--- src/Components/TodoInput.jsx
+++ src/Components/TodoInput.jsx
@@ -5,17 +5,27 @@ function TodoInput(props) {
   const { handleAddTodo } = props;
   const [inputValue, setInputValue] = useState('');
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      if (!inputValue) { return }
+      handleAddTodo(inputValue)
+      setInputValue('')
+    }
+  }
+
   return (
     <div className="input-container">
       <input value={inputValue} 
         onChange={(e) => {setInputValue(e.target.value)}} 
+        onKeyDown={handleKeyDown}
         placeholder="Add task" 
       />
       <button onClick={() => {
         if (!inputValue) { return }
         handleAddTodo(inputValue)
         setInputValue('')
-      }}>
+        }}
+      >
         <i className="fa-regular fa-square-plus"></i>
       </button>
     </div>
