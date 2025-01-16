ba8298b1658b3ae0f11b9556cbe399bca043a63c
Alexander Bryant <asbryant@me.com>
2025-01-15 17:08:54 -0700

    updated App and Header
    Added props to todo array, to find out length and pluralise text string for header

--- todo1/src/App.jsx
+++ todo1/src/App.jsx
@@ -4,12 +4,17 @@ import TodoInput from "./Components/TodoInput";
 import ToDoList from "./Components/ToDoList";
 
 function App() {
-
+  const todos = [
+    { input: 'Hello! Add your first todo!', complete: true },
+    { input: 'Get the groceries', complete: false },
+    { input: 'Take the dog out', complete: false },
+    { input: 'Wash the car', complete: true }
+  ]
   return (
     <>
-      <Header />
-      <Tabs />
-      <ToDoList />
+      <Header todos={todos} />
+      <Tabs todos={todos} />
+      <ToDoList todos={todos} />
       <TodoInput />
     </>
   )
--- todo1/src/Components/Header.jsx
+++ todo1/src/Components/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-function Header() {
+function Header(props) {
+  const { todos } = props;
+  const todosLength = todos.length;
+
+  const isTasksPlural = todos.length != 1;
+
+  const taskOrTasks = isTasksPlural ? 'tasks' : 'task';
+
   return (
     <header>
-      <h1>You have # open tasks.</h1>
+      <h1>You have {todosLength} open {taskOrTasks}.</h1>
     </header>
   )
 }
