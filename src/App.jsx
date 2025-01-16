bbfee121e48e440582dcb53e3b31a5e1d31e1468
Alexander Bryant <asbryant@me.com>
2025-01-16 13:03:32 -0700

    Update App.jsx
    Getting rid of placeholding todo object

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,6 @@ import ToDoList from "./Components/TodoList";
 import Reset from "./Components/Reset";
 
 function App() {
-/*   const todos = [
-    { input: 'Hello! Add your first todo!', complete: true },
-    { input: 'Get the groceries', complete: false },
-    { input: 'Take the dog out', complete: false },
-    { input: 'Wash the car', complete: true }
-  ] */
 
   const defaultTodo = [{ input: 'Hello! Add your first todo!', complete: false }]
 
