a043ea55cbe0f88a31d8e9f69256e691c296c3b4
Alexander Bryant <asbryant@me.com>
2025-01-16 09:23:54 -0700

    Filtering
    Filtering todos to show based on tab selection via Todo List

--- todo1/src/Components/TodoCard.jsx
+++ todo1/src/Components/TodoCard.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 
-function TodoCard() {
+function TodoCard(props) {
+  const { todoIndex, todos } = props;
+
+  const todo = todos[todoIndex];
+  console.log(todo);
+
   return (
-    <div></div>
+    <div className="card todo-item">
+      <p>{todo.input}</p>
+      <div className="todo-buttons">
+        <button disabled={todo.complete}>
+          <h6>Done</h6>
+        </button>
+        <button>
+          <h6>Delete</h6>
+        </button>
+      </div>
+    </div>
   )
 }
 
--- todo1/src/Components/TodoList.jsx
+++ todo1/src/Components/TodoList.jsx
@@ -3,12 +3,21 @@ import TodoCard from './TodoCard';
 
 function ToDoList(props) {
   const { todos } = props;
-
+  
   const tab = 'All';
+  const filterTodosList = tab === 'All' ? 
+  todos :
+  tab === 'Completed' ?
+  todos.filter(val => val.completed) :
+  todos.filter(val => !val.completed)
 
   return (
     <>
-      
+      {todos.map((todo, todoIndex) => {
+        return (
+          <TodoCard key={todoIndex} todoIndex={todoIndex} {...props} />
+        )
+      })}
     </>
   )
 }
