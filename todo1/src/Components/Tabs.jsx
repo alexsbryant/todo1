631b3bd0f4fc3ddc0c2d314879c1e4d2c963a572
Alexander Bryant <asbryant@me.com>
2025-01-15 17:15:33 -0700

    Update Tabs.jsx
    Javascript logic for filtering todo list updated

--- todo1/src/Components/Tabs.jsx
+++ todo1/src/Components/Tabs.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
-function Tabs() {
+function Tabs(props) {
+  const { todos } = props;
+
   const tabs = ['All', 'Open', 'Completed'];
   return (
     <nav className="tab-container">
       {tabs.map((tab, tabIndex) => {
+        const numOfTasks = tab === 'All' ? todos.length : 
+        tab === 'Open' ? 
+          todos.filter(val => !val.complete).length : 
+          todos.filter(val => val.complete).length
+
         return (
           <button 
           key={tabIndex}
